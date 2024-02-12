96e9ecd1f44dad76927861609625ff7a8d10015e
jonathandow <89482514+jonathandow@users.noreply.github.com>
2024-02-12 14:01:57 -0500

    Initial Commit

---
+++ SharpScale/SharpScaleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SharpScaleApp.swift
+//  SharpScale
+//
+//  Created by Jonathan Dow on 2/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SharpScaleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
