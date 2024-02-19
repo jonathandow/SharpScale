ca8037368eab8e47abe28543670b3f14bb97fb7b
jonathandow <89482514+jonathandow@users.noreply.github.com>
2024-02-19 13:18:25 -0500

    Implemented BTManager

---
+++ SharpScale/BTManager.swift
@@ -0,0 +1,94 @@
+import CoreBluetooth
+
+class BTManager: NSObject, CBCentralManagerDelegate, CBPeripheralDelegate {
+    var centralManager: CBCentralManager!
+    var raspberryPiPeripheral: CBPeripheral?
+    var recipeCharacteristic: CBCharacteristic?
+    var ingredientCharacteristic: CBCharacteristic?
+
+    let raspberryPiServiceUUID = CBUUID(string: "INSERT SERVICE UUID")
+    let recipeCharacteristicUUID = CBUUID(string: "INSERT RECIPE UUID")
+    let ingredientCharacteristicUUID = CBUUID(string: "INSERT INGREDIENT UUID")
+
+    override init() {
+        super.init()
+        centralManager = CBCentralManager(delegate: self, queue: nil)
+    }
+    func sendData(data: Data, forCharactersiticUUID uuid: CBUUID) {
+        guard let peripheral = raspberryPiPeripheral,
+              let characteristic = findCharacteristic(by: uuid, in: peripheral) else {
+            return
+        }
+        peripheral.writeValue(data, for: characteristic, type: .withResponse)
+    }
+    
+    private func findCharacteristic(by uuid: CBUUID, in peripheral: CBPeripheral) -> CBCharacteristic? {
+        guard let services = peripheral.services else { return nil }
+        for service in services {
+            guard let characteristics = service.characteristics else { continue }
+            for characteristic in characteristics {
+                if characteristic.uuid == uuid {
+                    return characteristic
+                }
+            }
+        }
+        return nil
+    }
+    
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        if central.state == .poweredOn {
+            centralManager.scanForPeripherals(withServices: [raspberryPiServiceUUID], options: nil)
+        } else {
+            print("Bluetooth is not available.")
+        }
+    }
+
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String: Any], rssi RSSI: NSNumber) {
+        raspberryPiPeripheral = peripheral
+        raspberryPiPeripheral?.delegate = self
+        centralManager.stopScan()
+        centralManager.connect(raspberryPiPeripheral!, options: nil)
+    }
+
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        peripheral.discoverServices([raspberryPiServiceUUID])
+    }
+
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+        guard let services = peripheral.services else { return }
+        for service in services {
+            peripheral.discoverCharacteristics([recipeCharacteristicUUID, ingredientCharacteristicUUID], for: service)
+        }
+    }
+
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
+        guard let characteristics = service.characteristics else { return }
+        for characteristic in characteristics {
+            if characteristic.uuid == recipeCharacteristicUUID {
+                recipeCharacteristic = characteristic
+            } else if characteristic.uuid == ingredientCharacteristicUUID {
+                ingredientCharacteristic = characteristic
+            }
+        }
+    }
+
+    func sendRecipeData(_ data: Data) {
+        guard let characteristic = recipeCharacteristic else { return }
+        raspberryPiPeripheral?.writeValue(data, for: characteristic, type: .withResponse)
+    }
+
+    func sendIngredientData(_ data: Data) {
+        guard let characteristic = ingredientCharacteristic else { return }
+        raspberryPiPeripheral?.writeValue(data, for: characteristic, type: .withResponse)
+    }
+
+    func readRecipeData() {
+        guard let characteristic = recipeCharacteristic else { return }
+        raspberryPiPeripheral?.readValue(for: characteristic)
+    }
+
+    func readIngredientData() {
+        guard let characteristic = ingredientCharacteristic else { return }
+        raspberryPiPeripheral?.readValue(for: characteristic)
+    }
+}
