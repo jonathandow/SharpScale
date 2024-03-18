026398f0c6028c019979ebc63ca2697d37696157
jonathandow <89482514+jonathandow@users.noreply.github.com>
2024-03-18 13:15:42 -0400

    Update BLEService.py - switched back to pybleno

--- BLEService.py
+++ BLEService.py
@@ -1,25 +1,68 @@
-from bleak import BleakServer
-import asyncio
+import pybleno
+import array
 
-class SimpleService:
-    UUID = "77670a58-1cb4-4652-ae7d-2492776d303d"
 
+SERVICE_UUID = '77670a58-1cb4-4652-ae7d-2492776d303d'
+
+CHARACTERISTIC_UUID = '13092a53-7511-4ae0-8c9f-97c84cfb5d9a'
+
+class Characteristic(pybleno.Characteristic):
     def __init__(self):
-        self.value = bytearray("Data to send", 'utf-8')
+        pybleno.Characteristic.__init__(self, {
+            'uuid': CHARACTERISTIC_UUID,
+            'properties': ['read', 'write', 'notify'],
+            'value': None
+        })
 
-    async def read(self):
-        return self.value
+        self._value = array.array('B', [0] * 0)
 
-    async def write(self, value):
-        self.value = value
-        print(f"Write request received: {value}")
+    def onReadRequest(self, offset, callback):
+        print('Read request received')
 
-async def main():
-    service = SimpleService()
-    async with BleakServer(service, service_uuids=[service.UUID]) as server:
-        await server.start()
-        print("Server is now running")
-        await asyncio.sleep(float("inf")) 
+        callback(self.RESULT_SUCCESS, self._value)
 
-if __name__ == "__main__":
-    asyncio.run(main())
+    def onWriteRequest(self, data, offset, withoutResponse, callback):
+        self._value = data
+        print('Write request received:', data)
+
+        if len(data) > 0:
+            print('Data received:', data)
+
+        callback(self.RESULT_SUCCESS)
+
+    def onSubscribe(self, maxValueSize, updateValueCallback):
+        print('Subscribed to')
+        self._updateValueCallback = updateValueCallback
+
+    def onUnsubscribe(self):
+        print('Unsubscribed from')
+        self._updateValueCallback = None
+
+bleno = pybleno.Bleno()
+
+def onStateChange(state):
+    print('on -> stateChange: ' + state)
+
+    if state == 'poweredOn':
+        bleno.startAdvertising('Peripheral', [SERVICE_UUID])
+    else:
+        bleno.stopAdvertising()
+
+bleno.on('stateChange', onStateChange)
+
+def onAdvertisingStart(error):
+    print('on -> advertisingStart: ' + ('error ' + error if error else 'success'))
+
+    if not error:
+        bleno.setServices([
+            pybleno.Service({
+                'uuid': SERVICE_UUID,
+                'characteristics': [
+                    Characteristic()
+                ]
+            })
+        ])
+
+bleno.on('advertisingStart', onAdvertisingStart)
+
+bleno.start()
