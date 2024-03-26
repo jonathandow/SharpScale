91568828ae95aaa04149a4f9f63c5762ba31f92e
jonathandow <89482514+jonathandow@users.noreply.github.com>
2024-03-26 12:54:38 -0400

    Update BLEService.py

--- BLEService.py
+++ BLEService.py
@@ -1,57 +1,60 @@
-from bluetooth import *
-import time
 from pybleno import Bleno, BlenoPrimaryService, Characteristic
-
+import time
+import sys
+import signal
 
 SERVICE_UUID = '77670a58-1cb4-4652-ae7d-2492776d303d'
 CHARACTERISTIC_UUID = '13092a53-7511-4ae0-8c9f-97c84cfb5d9a'
-
 DEVICE_NAME = "RaspberryPi5_BLE"
 
-def setup_service():
-    try:
-        print("1")
-        server_sock = BluetoothSocket(RFCOMM)
-        print("2")
-        server_sock.bind(("", PORT_ANY))
-        print("3")
-        server_sock.listen(1)
-        print("4")
+class MyChar(Characteristic):
+	def __init__(self):
+		Characteristic.__init__(self, {
+			'uuid': CHARACTERISTIC_UUID,
+			'properties': ['read', 'write', 'notify'],
+			'value': None
+		})
+	
+	def onReadRequest(self, offset, callback):
+		print("Read Request")
+		data = "Hello!"
+		callback(Characteristic.RESULT_SUCCESS, bytearray(data, 'utf-8'))
+		
+bleno = Bleno()
+myCharacteristic = MyChar()
 
-        advertise_service(
-            server_sock,
-            DEVICE_NAME,
-            service_id=SERVICE_UUID,
-            service_classes=[SERVICE_UUID, SERIAL_PORT_CLASS],
-            profiles=[SERIAL_PORT_PROFILE],
-        )
-        print("5")
-        print("Waiting for connection...")
-        client_sock, address = server_sock.accept()
-        print(f"Accepted connection from {address}")
+def on_start(success):
+	print("Bleno Starting... {}".format(success))
+	if success:
+		service = BlenoPrimaryService({
+			'uuid': SERVICE_UUID,
+			'characteristics': [myCharacteristic]
+		})
+		bleno.setServices([service])
+		
+def on_state_change(state):
+	if state == 'poweredOn':
+		bleno.startAdvertising(DEVICE_NAME, [SERVICE_UUID])
+	else:
+		bleno.stopAdvertising()
 
-        return client_sock, server_sock
+def signal_handler(signal, frame):
+    print('You pressed Ctrl+C!')
+    bleno.stopAdvertising()
+    bleno.disconnect()
 
-    except btcommon.BluetoothError as e:
-        print("Bluetooth error:", e)
-        return None, None
+    sys.exit(0)
 
-if __name__ == "__main__":
-    print("Main 1")
-    client_sock, server_sock = setup_service()
-    print("Main 2")
-    if client_sock is not None:
-        try:
-            while True:
-                data = "Hello, iOS!"
-                client_sock.send(data.encode())
-                print(f"Sent data: {data}")
+signal.signal(signal.SIGINT, signal_handler)
+		
+bleno.on('stateChange', on_state_change)
+bleno.on('advertisingStart', on_start)
 
-                time.sleep(1)
-
-        except KeyboardInterrupt:
-            print("Peripheral stopped")
-
-        finally:
-            client_sock.close()
-            server_sock.close()
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    pass
+finally:
+    bleno.stopAdvertising()
+    bleno.disconnect()
