d82fab41a0f5f9f282c02543c2334981916f359a
jonathandow <89482514+jonathandow@users.noreply.github.com>
2024-04-16 16:00:30 -0400

    Update excelWriter.py

--- excelWriter.py
+++ excelWriter.py
@@ -8,11 +8,27 @@ def read_db(filename):
 def writeExcel(data, filename, sheet):
 	df = pd.DataFrame(data)
 	df.to_excel(filename, sheet_name=sheet, index=False)
+	
+def classification(steps, classifications):
+	c = []
+	for step in steps.split("."):
+		first = step.strip().split(" ")[0]
+		if first in classifications:
+			c.append(classifications[first])
+		else:
+			c.append("0")
+	return ", ".join(c)
 
 def process_db(json_file):
 	data = read_db(json_file)
 	recipes = data.get('recipes', [])
 	ingredients = data.get('ingredients', [])
+	r_names = [recipe['name'] for recipe in recipes]
+	classifications = {"Mix": "1", "Add": "1", "Combine": "1", "Measure": "1"}
+	
+	for recipe in recipes:
+		recipe['classification'] = classification(recipe.get('steps', ''), classifications)
+	 
 	
 	with pd.ExcelWriter('received_db.xlsx', engine='openpyxl') as writer:
 		writeExcel(ingredients, writer, 'Ingredients')
