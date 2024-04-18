fdc72530ea809f9b66978a399dd7a1e4bf30f3dd
jonathandow <89482514+jonathandow@users.noreply.github.com>
2024-04-18 16:05:35 -0400

    Update excelWriter.py

--- excelWriter.py
+++ excelWriter.py
@@ -1,5 +1,6 @@
 import json
 import re
+import os
 import pandas as pd
 
 def read_db(filename):
@@ -44,13 +45,56 @@ def process_db(json_file):
 		
 	print('Wrote data to received_db.xlsx')
 
+def extractData(excel_file):
+	if not os.path.exists(excel_file):
+		print("Excel File Not Found.")
+		return
+	try:
+		ingredient_data = pd.read_excel(excel_file, 'Ingredients')
+		recipe_data = pd.read_excel(excel_file, 'Recipes')
+	except Exception as e:
+		print(f"Failed to read Excel file: {str(e)}")
+		return
+	
+	results = []
+	for idx, row in recipe_data.iterrows():
+		recipe_name = row['name']
+		steps = row['steps']
+		pattern = re.compile(r'\.\s*(?=[A-Z])|\.$')
+		step_list = pattern.split(steps)
+		classifications = row['classification']
+		measure_nums = []
+		class_list = []
+		class_idx = 0
+		for c in classifications.split(","):
+			class_list.append(c)
+		for step in step_list:
+			print(step)
+			step = step.strip()
+			num = re.findall(r'[-+]?[0-9]*\.?[0-9]+', step)
+			if num:
+				measure_nums.append(num[0])
+			else:
+				measure_nums.append(" ")
+			
+		results.append({
+			'Recipe Name': recipe_name,
+			'Steps': step_list,
+			'Classifications': class_list,
+			'Measurement Numbers': measure_nums
+		})
+
+	return results
+			
 def clear_file(filename):
 	with open(filename, 'r+') as f:
 		f.truncate(0)
-
+	
 if __name__ == "__main__":
 	clear_file('received_db.xlsx')
 	process_db("SSData.db")
 	with open("SSData.db", 'r+') as f:
 		f.truncate(0)
+	res = extractData("received_db.xlsx")
+	print(res)
 
